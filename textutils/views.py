11bc4aaa8d90597b910a1a5163429b99b9a19908
Rajneesh Yadav <rajneesh.ai11@gmail.com>
2023-08-02 19:16:17 +0530

    Emp-Mgmt-System

---
+++ textutils/views.py
@@ -0,0 +1,9 @@
+# I have created this file- Raj
+
+'''from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("<h1>Hello! Rajneesh</h1>")
+
+def about(request):
+    return HttpResponse("About Rajneesh")'''
